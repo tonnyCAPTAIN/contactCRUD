7c301be14e239637d62e13678593a346128e13e9
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2023-08-14 07:40:38 +0300

    mongo db connection

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,10 +1,12 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel")
 //@desc get all contacts
 //@route GET /api/contacts
 //@access public
 
 const getContacts =asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Get all contact"});
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
 });
 
 
@@ -20,7 +22,13 @@ const createContact = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error("All fields are mandatory");
     }
-    res.status(201).json({ message: "Create contact"});
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+    });
+
+    res.status(201).json(contact);
 });
 
 
